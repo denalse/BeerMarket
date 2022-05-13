82161c20d7985c963c5272897e05ce3d53b8e8de
denalse <denaleewonders@gmail.com>
2022-05-13 14:12:53 +0800

    new

--- src/main/resources/static/style.js
+++ src/main/resources/static/style.js
@@ -37,25 +37,3 @@
             reader.readAsDataURL(file);
         }
     });
-
-// function for upload and display image      
-    // const image_input = document.querySelector("#image_input");
-    // var uploaded_image = "";
-
-    // image_input.addEventListener("change", function() {
-    //     const reader = new FileReader();
-    //     reader.addEventListener("load", () => {
-    //       uploaded_image = reader.result;
-    //       document.querySelector("#display_image").style.backgroundImage ='url(${uploaded_image})';
-    //     // console.log(image_input.value);
-    //     });
-    //     reader.readAsDataURL(this.files[0]);
-    //   })
-
-      
-    
-    
-
-
-
-    
